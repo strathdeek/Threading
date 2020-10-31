0ea9314043336d2ebbd3516c512c5b31ca96b7e6
Kevin Strathdee <strathdee.kevin@gmail.com>
2020-10-31 11:50:32 +0100

    producer boilerplate

---
+++ Threading/Utilities/Producer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading.Utilities
+{
+    public class Producer
+    {
+        private bool stopped = false;
+        private readonly Sequencer sequencer;
+
+        public Producer()
+        {
+            var thread = new Thread(() =>
+            {
+                while (!stopped)
+                {
+                    var number = 1; //< generate new number from the sequence>
+                    sequencer.PerformAsync((queue) => {
+                        queue.Enqueue(number);
+                     }); // change it however needed to make it work correctly
+
+                    //< wait the needed delay AFTER adding of the number is completed >
+                }
+            });
+        thread.Start();
+        }
+
+        public void Stop()
+        {
+            stopped = true;
+        }
+    }
+}
